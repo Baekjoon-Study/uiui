4886981630d403dbff39a8fab1f0792721b50400
GHY25 <hygga.1119@gmail.com>
2024-09-06 11:48:10 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+    int grade;
+    scanf("%d",&grade);
+
+    if (90<=grade) printf("A");
+    else if ((80<=grade) && (grade<=89)) printf("B");
+    else if ((70<=grade) && (grade<=79)) printf("C");
+    else if ((60<=grade) && (grade<=69)) printf("D");
+    else printf("F");
+
+    return 0;
+}
